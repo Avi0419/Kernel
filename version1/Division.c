657275a95e4ae7cda2df5c7b90f9e4eb77f9d1f6
Nupura <torvekar.nupura@gmail.com>
2017-10-26 04:23:21 +0530

    version1

---
+++ version1/Division.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+
+#include<conio.h>
+
+
+void main()
+{
+
+
+int one, two, div;
+
+
+printf("Enter first number - ");
+
+
+scanf("%d",&one);
+
+
+printf("Enter second number - ");
+
+
+scanf("%d",&two);
+
+
+div = one / two;
+
+
+printf("The divison of numbers %d and %d is %d",one,two,div);
+
+
+getch();
+
+
+}
