3fd96a7ee47cea83cebc2075f43691a0962a6ff9
Avi0419 <Avinashbhujbal1989@gmail.com>
2017-10-25 10:00:32 +0530

    Version 1

--- Sum.c
+++ Sum.c
@@ -1,4 +1,18 @@
+#include <stdio.h>
+int main()
+{
+    int firstNumber, secondNumber, sumOfTwoNumbers;
+    
+    printf("Enter two integers: ");
 
+    // Two integers entered by user is stored using scanf() function
+    scanf("%d %d", &firstNumber, &secondNumber);
 
+    // sum of two numbers in stored in variable sumOfTwoNumbers
+    sumOfTwoNumbers = firstNumber + secondNumber;
 
-this application is for adding data set.
+    // Displays sum      
+    printf("%d + %d = %d", firstNumber, secondNumber, sumOfTwoNumbers);
+
+    return 0;
+}
